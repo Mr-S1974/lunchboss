70c3429a05862dccd886f7c3f869b1ba33f15cd9
서현석 <shs010393@gmail.com>
2026-03-31 05:34:54 +0000

    Add Microsoft Clarity tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,17 @@ export default function RootLayout({
   return (
     <html lang="ko" suppressHydrationWarning>
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "w47wx3mav9");
+            `,
+          }}
+        />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-TXHYQWL2B9"></script>
         <script
           dangerouslySetInnerHTML={{
