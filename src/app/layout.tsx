fdae5516c7d44de939cf11787e294fe78239d502
서현석 <shs010393@gmail.com>
2026-03-31 06:05:41 +0000

    Add AdSense site verification setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,8 @@ export default function RootLayout({
   return (
     <html lang="ko" suppressHydrationWarning>
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4269966166949156" crossOrigin="anonymous"></script>
+        <meta name="google-adsense-account" content="ca-pub-4269966166949156" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
